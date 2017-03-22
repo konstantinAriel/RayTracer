4aa5e32d32288bd1f1630644af8e1bdb391e6baf
konstantin <konstantinsh@ariel.ac.il>
2017-03-22 12:45:18 +0200

    ver 0.0.3

--- scr/MainParam.py
+++ scr/MainParam.py
@@ -2,37 +2,34 @@ import pandas as pd
 import numpy as np
 
 class Parametrs:
-    def __index__(self,path, paramName):
-        self.path = path
+    def __init__(self, paramName):
+        self.path = "/home/konstantin/PycharmProjects/RayTracer/files/settingsfiles/testSheets4py.xls"
         self.paramName = paramName
         self.paramFile = pd.ExcelFile(self.path)
         self.sheetsNames = self.paramFile.sheet_names
         self.mNumber = len(self.sheetsNames) - 3
-        self.paramObj = self.getLineParam()
-
+        self.paramTable = self.getParam()
 
     def getParam(self):
         self.setParam(self.paramFile, self.paramName)
-        return self.paramObj
-
+        return self.paramTable
 
     def setParam(self, paramDataFile, paramName):
-        self.paramObj = paramDataFile.parse(sheetname=paramName)
+        self.paramTable = paramDataFile.parse(sheetname=paramName)
 
-
-# def getMirrorList():
-#     count = 0
-#     mirrorDict = {}
-#     print(mirrorDict)
-#     for i in sysParam.Rin:
-#             if np.isnan(i):
-#               break
-#             else:
-#                 mirrorList = []
-#                 position = 0
-#                 for j in range(int(sysParam.Rin[count]), int(sysParam.Rout[count]) + 1):
-#                     mirrorList.insert(position, 'mirror' + str(j))
-#                     position = position + 1
-#                 mirrorDict['mirrorDict'+str(count+1)] = mirrorList
-#                 count = count + 1
-#     return mirrorDict
+    def getMirrorList(self, sysParam):
+        print("paramTable", sysParam.Rin)
+        count = 0
+        mirrorDict = {}
+        for i in sysParam.Rin:
+            if np.isnan(i):
+                break
+            else:
+                mirrorList = []
+                position = 0
+                for j in range(int(sysParam.Rin[count]), int(sysParam.Rout[count]) + 1):
+                    mirrorList.insert(position, 'mirror' + str(j))
+                    position += 1
+                mirrorDict['mirrorDict'+str(count+1)] = mirrorList
+                count += 1
+        return mirrorDict
--- scr/main.py
+++ scr/main.py
@@ -1,7 +1,18 @@
 import  numpy as np
 from myPackages.readExelfile.ReadXls import ReadXls
+from scr.MainParam import Parametrs
+
+#path = "/home/kosta/RayTracer/RayTracer/files/settingsfiles/testSheets4py.xls"
+path = "/home/konstantin/PycharmProjects/RayTracer/files/settingsfiles/testSheets4py.xls"
+
+tLine = Parametrs("LineParam")
+sys = Parametrs("SysParam")
+Rin = Parametrs( "RaysIn")
 
 
-path = "/home/kosta/RayTracer/RayTracer/files/settingsfiles/testSheets4py.xls"
+mirrorDictMain = Rin.getMirrorList(sys.paramTable)
 
-
+for mirrorDictSub in mirrorDictMain.keys():
+    print(mirrorDictSub)
+    for mirrorList in mirrorDictSub:
+        print(mirrorList)
